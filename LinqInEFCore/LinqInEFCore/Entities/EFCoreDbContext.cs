019936efdd45ecb509c79e13a1c3edcd066f61ad
ArunBadoniMarketLink <arun.badoni@dxc.com>
2024-05-29 15:45:42 +0530

    Added MyFolder and its sub-folder code

---
+++ LinqInEFCore/LinqInEFCore/Entities/EFCoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqInEFCore.Entities
+{
+    internal class EFCoreDbContext:DbContext
+    {
+        //Constructor calling the Base DbContext Class Constructor
+        public EFCoreDbContext() : base()
+        {
+        }
+        //OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //To Display the Generated the Database Script
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            //Configuring the Connection String
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EFCoreDB1;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        //OnModelCreating() method is used to configure the model using ModelBuilder Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //use this to configure the model
+        }
+        //Adding Domain Classes as DbSet Properties
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+}
---
+++ LinqInEFCore/LinqInEFCore/Program.cs
@@ -0,0 +1,54 @@
+﻿using LinqInEFCore.Entities;
+using System.Runtime.CompilerServices;
+
+namespace LinqInEFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var context = new EFCoreDbContext();
+
+                var joinUsingMS = context.Students.Join(context.Standards,
+                    student => student.Standard.StandardId,
+                    Standard => Standard.StandardId,
+                    (student,standard)=>new
+                    {
+                        StudentName = student.FirstName +" "+student.LastName,
+                        standrdId = standard.StandardId,
+                        StudentHeight = student.Height
+                    }).ToList();
+
+                foreach (var student in joinUsingMS)
+                {
+                    Console.WriteLine($"Name : {student?.StudentName}, Height : {student?.StudentHeight}, Standard : {student?.standrdId}");
+                }
+
+                Console.WriteLine("---------------------------------------");
+
+                var joinUsigQS = (from student in context.Students
+                                  join standard in context.Standards
+                                  on student.Standard.StandardId equals standard.StandardId
+                                  select new
+                                  {
+                                      sName = student.FirstName+" "+student.LastName,
+                                      sHeight = student.Height,
+                                      standardId = standard.StandardId
+                                  }).ToList();
+
+                foreach (var student in joinUsigQS)
+                {
+                    Console.WriteLine($"Name : {student?.sName}, Height : {student?.sHeight}, Standard : {student.standardId}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
