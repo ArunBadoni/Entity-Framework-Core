66bdafb7256fee56027740a5d5340c14a267540b
ArunBadoniMarketLink <arun.badoni@dxc.com>
2024-05-29 15:33:10 +0530

    Added MyFolder and its sub-folder code

---
+++ FirstEFCoreApp/FirstEFCoreApp/Entities/EFCoreDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEFCoreApp.Entities
+{
+    public class EFCoreDbContext : DbContext
+    {
+        public EFCoreDbContext():base()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //To Display the Generated the Database Script
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EFCoreDB1;
+            //Trusted_Connection=True;TrustServerCertificate=True;");
+            var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var configuration = configurationBuilder.GetSection("ConnectionStrings");
+            var connectionString = configuration["SQLServerConnection"] ?? null;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+}
---
+++ FirstEFCoreApp/FirstEFCoreApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using FirstEFCoreApp.Entities;
+
+namespace FirstEFCoreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /* For Insert new Student
+                Student student = new Student()
+                {
+                    FirstName = "Chatrapati",
+                    LastName = "Shivaji",
+                    DateOfBirth = DateTime.Now.AddYears(-14),
+                    Height = 13,
+                    Weight = 17,
+                    Standard = null
+                };
+                var context = new EFCoreDbContext();
+                //context.Add(student);
+                //context.Students.Add(student);
+                context.Add<Student>(student);
+
+                //Now the Entity State will be in Added State
+                Console.WriteLine($"Before SaveChanges Entity State: {context.Entry(student).State}");
+
+                context.SaveChanges();
+
+                //Now the Entity State will change from Added State to Unchanged State
+                Console.WriteLine($"After SaveChanges Entity State: {context.Entry(student).State}");
+
+                Console.WriteLine("Student Data Saved Successfully");
+                Console.ReadLine();
+                */
+
+                /* For Update Existing Student
+                var context = new EFCoreDbContext();
+                Student student = context.Students.Find(7);
+                if(student != null) 
+                {
+                    Console.WriteLine($"Entity State Before Update:{context.Entry(student).State}");
+                    student.FirstName = "Deleting";
+                    student.LastName = "Entry";
+                    Console.WriteLine($"Entity State After Update:{context.Entry(student).State}");
+                    context.SaveChanges();
+                    Console.WriteLine($"Entity State After SaveChanges:{context.Entry(student).State}");
+                    Console.WriteLine("Student Entry Updated Successfully");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("No Student Found");
+                }
+                */
+
+                var context = new EFCoreDbContext();
+                Student student = context.Students.Find(7);
+                if (student != null)
+                {
+                    Console.WriteLine($"Entity State Before Update:{context.Entry(student).State}");
+                    context.Students.Remove(student);
+
+                    Console.WriteLine($"Entity State After Update:{context.Entry(student).State}");
+                    context.SaveChanges();
+                    Console.WriteLine($"Entity State After SaveChanges:{context.Entry(student).State}");
+                    Console.WriteLine("Student Entry Deleted Successfully");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("No Student Found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : "+ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
