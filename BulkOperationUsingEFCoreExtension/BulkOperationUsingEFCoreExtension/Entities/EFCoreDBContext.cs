884ab6bae269dc1f3e665bcbeedb2db2e4d8b9bb
unknown <badoniarun518@gmail.com>
2024-05-30 15:32:34 +0530

    Added MyFolder and its sub-folder code

---
+++ BulkOperationUsingEFCoreExtension/BulkOperationUsingEFCoreExtension/Entities/EFCoreDBContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkOperationUsingEFCoreExtension.Entities
+{
+    internal class EFCoreDBContext:DbContext
+    {
+        public EFCoreDBContext()
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var ConfigurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var config = ConfigurationBuilder.GetSection("ConnectionStrings");
+            var connString = config["SQLServerConnection"] ?? null;
+            optionsBuilder.UseSqlServer(connString);
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ BulkOperationUsingEFCoreExtension/BulkOperationUsingEFCoreExtension/Program.cs
@@ -0,0 +1,54 @@
+﻿using BulkOperationUsingEFCoreExtension.Entities;
+
+namespace BulkOperationUsingEFCoreExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                var context = new EFCoreDBContext();
+                //Create a List of Students and Bulk Insert 
+                /*
+                List<Student> newStudents = new List<Student>() {
+                    new Student() { FirstName = "Pranaya", LastName = "Rout", Branch= "CSE" },
+                    new Student() { FirstName = "Hina", LastName = "Sharma", Branch= "CSE"  },
+                    new Student() { FirstName = "Anurag", LastName= "Mohanty", Branch= "CSE" },
+                    new Student() { FirstName = "Prity", LastName= "Tiwary", Branch= "ETC" }
+                };
+                context.BulkInsert(newStudents);
+                Console.WriteLine("Student data inserted successfully");*/
+
+
+                //Bulk Update Code
+                /*var students =context.Students.Where(s => s.Branch == "CSE");
+                foreach (var student in students)
+                {
+                    student.LastName += " changed";
+                }
+                context.BulkUpdate(students);
+                Console.WriteLine("Student data updated successfully");*/
+
+                var students = context.Students.Where(s => s.Branch == "CSE");
+                context.BulkDelete(students);
+                Console.WriteLine("Student data updated successfully");
+                GetStudents("CSE", context);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public static void GetStudents(string Branch,EFCoreDBContext context)
+        {
+            var students = context.Students.Where(s=>s.Branch==Branch);
+            Console.WriteLine("Student having branch as CSE:\n");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Student Id : {student.StudentId}\n StudentName : {student.FirstName} {student.LastName}\n Branch : {student.Branch}\n\n");
+            }
+        }
+    }
+}
